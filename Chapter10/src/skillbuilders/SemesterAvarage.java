7e4cfbb5842534879d9910ceb888f8e084e99e92
1100062345 <1100062345@L815WD220028.edu.cbe.ab.ca>
2024-09-17 14:24:30 -0600

    2:24PM

--- Chapter10/src/skillbuilders/SemesterAvarage.java
+++ Chapter10/src/skillbuilders/SemesterAvarage.java
@@ -2,8 +2,9 @@ package skillbuilders;
 
 public class SemesterAvarage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
